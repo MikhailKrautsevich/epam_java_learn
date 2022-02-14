d71141c275ade821a07e56c00f8544c7ea90547e
Misha <mihail4egevara@gmail.com>
2022-02-14 18:28:18 +0300

    Literals

---
+++ src/fundamentals/Literals.java
@@ -0,0 +1,17 @@
+package fundamentals;
+
+public class Literals {
+    public static void main(String[] args) {
+        test2();
+    }
+
+    private static void test2() {
+//        float f = 7.0;
+//        char c = "z";
+//        byte b = 255;
+//        boolean n = null;
+        int i = 32565;
+        int j = 'ъ';
+        System.out.println(j);
+    }
+}
