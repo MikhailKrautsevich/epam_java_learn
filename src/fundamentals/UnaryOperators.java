a2e3b1cfd8f5ecd5dcc85d1ebdbc4965490e1173
Misha <mihail4egevara@gmail.com>
2022-02-18 15:13:17 +0300

    Unary Operators

---
+++ src/fundamentals/UnaryOperators.java
@@ -0,0 +1,15 @@
+package fundamentals;
+
+public class UnaryOperators {
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    private static void test1(){
+        long a = Long.remainderUnsigned(-2, 3);
+        int b = Integer.remainderUnsigned(-2, 3);
+        int c = -2%3;
+        System.out.println(a + " " + b + " " + c);
+    }
+}
