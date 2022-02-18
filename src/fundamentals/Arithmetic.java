97f5c92bc0a60739ed571f308deee959e160a66c
Misha <mihail4egevara@gmail.com>
2022-02-18 13:22:05 +0300

    Arithmetic

---
+++ src/fundamentals/Arithmetic.java
@@ -0,0 +1,40 @@
+package fundamentals;
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        test1();
+        test2();
+        test3();
+    }
+
+    private static void test1() {
+        int a = 129;
+        byte b = 3;
+        Object ab = (byte) a + b;
+        System.out.println(ab.getClass().getName() + " value " + ab);
+    }
+
+    private static void test2() {
+        long a = 2147483647000L;
+        int b = 1;
+        b += a;
+        Object ab = b;
+        System.out.println(ab.getClass().getName() + " value " + ab);
+    }
+
+    private static void test3() {
+        char i = '1';
+        Quest.result(i);
+        System.out.println(i + " " + Quest.j);
+    }
+
+    static class Quest {
+        static int j = 2;
+
+        public static void result(int i) {
+            i *= 10;
+            j += 2;
+        }
+    }
+}
